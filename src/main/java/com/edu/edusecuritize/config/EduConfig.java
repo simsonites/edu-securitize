447effd6e8992fb9e2f32e4cd3bf058e3d1b7bdd
simsonites <samfeminite@gmail.com>
2021-10-12 00:14:03 +0700

    Override the default user and password for authentication

--- src/main/java/com/edu/edusecuritize/config/EduConfig.java
+++ src/main/java/com/edu/edusecuritize/config/EduConfig.java
@@ -2,14 +2,51 @@ package com.edu.edusecuritize.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-public class EduConfig {
+public class EduConfig extends WebSecurityConfigurerAdapter {
+/*
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var userDetailsService = new InMemoryUserDetailsManager();
+        var user = User.withUsername("sam")
+                .password("123456")
+                .authorities("read")
+                .build();
+        userDetailsService.createUser(user);
+        return userDetailsService;
+    }
 
     @Bean
-    public UserDetailsService userDetailsService(){
-        return new InMemoryUserDetailsManager();
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }*/
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.authorizeRequests().anyRequest().permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        var userDetailsService = new InMemoryUserDetailsManager();
+        var user = User.withUsername("sam")
+                .password("12345")
+                .authorities("read")
+                .build();
+        userDetailsService.createUser(user);
+
+        auth.userDetailsService(userDetailsService)
+            .passwordEncoder(NoOpPasswordEncoder.getInstance());
+
     }
 }
